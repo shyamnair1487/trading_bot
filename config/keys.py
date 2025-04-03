32bda8d27cdc8590870fbdf127dfd15eae849f64
shyamnair1487 <shyamnair1487@gmail.com>
2025-04-03 15:43:38 +0800

    Delete config/keys.py

--- config/keys.py
+++
@@ -1,5 +0,0 @@
-# NEVER commit this file to version control!
-# Add to .gitignore
-
-BINANCE_API_KEY = 'qKLu20YKOfcbyPMXjPcbWwMLO8vO2CbA7umqkwJuqrwx2stj1aUd3iJZiGTc57qx'
-BINANCE_API_SECRET = 'unJi3EEcbsebqTZ4U6LwRzAhcwSRmP6xX0B4v2hb4COpnJn2qsaHpYIw6NwXHDNL'
